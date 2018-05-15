08ac99759567c1ba01bb23de2ebebd5b54ec1524
Maubert Lucie <luspam@spam.com>
2018-05-15 12:39:49 +0200

    convertisseur + tests

---
+++ projetequipe3/src/test/java/projetequipe3/test/TestConvertisseur.java
@@ -0,0 +1,33 @@
+package projetequipe3.test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import projetequipe3.Convertisseur;
+
+public class TestConvertisseur {
+	
+	private Convertisseur convertisseur;
+	
+	@Before
+	public void init() {
+		convertisseur = new Convertisseur();
+	}
+	
+	@Test
+	public void testEuroToFranc() {
+		double montantEnFranc = convertisseur.euroToFranc(15.0);
+		Assert.assertEquals(montantEnFranc, 98.0, 2.0);
+		
+	}
+	
+	@Test
+	public void testFrancToEuro() {
+		double montantEnEuro = convertisseur.francToEuro(98.0);
+		Assert.assertEquals(montantEnEuro, 15.0, 2.0);
+		
+	}
+
+}
