b588169dee1c75b3e1d66132fed00317656274c7
Maubert Lucie <luspam@spam.com>
2018-05-15 10:14:57 +0200

    version test

--- projetequipe3/src/main/java/projetequipe3/AppLucie.java
+++ projetequipe3/src/main/java/projetequipe3/AppLucie.java
@@ -5,7 +5,7 @@ public class AppLucie {
 	public static void main(String[] args) {
 
 
-		System.out.println("Bonsoir, cher voisin. Comment vas-tu ?");
+		System.out.println("Ceci est un test");
 
 	}
 
