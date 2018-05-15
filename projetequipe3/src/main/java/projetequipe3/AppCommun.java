b639fc7d8414b074ee588ace94c7f50457ada12a
Maubert Lucie <luspam@spam.com>
2018-05-15 09:30:49 +0200

    test 2

--- projetequipe3/src/main/java/projetequipe3/AppCommun.java
+++ projetequipe3/src/main/java/projetequipe3/AppCommun.java
@@ -4,7 +4,7 @@ public class AppCommun {
 
 	public static void main(String[] args) {
 		System.out.println("bonjour bonjour");
-		System.out.println("oeoeoeoeoe");
+		System.out.println("comment tu vas ?");
 
 	}
 
