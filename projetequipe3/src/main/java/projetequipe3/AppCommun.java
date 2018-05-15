e785598fff23110d47db26642c0b7cbf438d7d94
Charretier Franck <franckcharretier@yahoo.fr>
2018-05-15 10:01:24 +0200

    version yeah

--- projetequipe3/src/main/java/projetequipe3/AppCommun.java
+++ projetequipe3/src/main/java/projetequipe3/AppCommun.java
@@ -4,7 +4,7 @@ public class AppCommun {
 
 	public static void main(String[] args) {
 		System.out.println("bonjour bonjour");
-		System.out.println("comment tu vas ?");
+		System.out.println("yeah!!!");
 
 	}
 
