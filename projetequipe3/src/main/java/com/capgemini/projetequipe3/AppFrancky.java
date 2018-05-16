88ee7ff4cdebdaf81b7e5c68f24fd41383b19a6b
Maubert Lucie <luspam@spam.com>
2018-05-16 14:13:23 +0200

    logger 2

--- projetequipe3/src/main/java/com/capgemini/projetequipe3/AppFrancky.java
+++ projetequipe3/src/main/java/com/capgemini/projetequipe3/AppFrancky.java
@@ -1,10 +1,15 @@
 package com.capgemini.projetequipe3;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AppFrancky {
+	
+	private static Logger logger = LoggerFactory.getLogger(AppFrancky.class);
 
 	public static void main(String[] args) {
-		System.out.println("ça va bien et toiiiiiiiiiiiiiiiiiiii ma chère voisine?");
-		System.out.println("kazama");
+		logger.info("ça va bien et toiiiiiiiiiiiiiiiiiiii ma chère voisine?");
+		logger.info("kazama");
 
 	}
 
