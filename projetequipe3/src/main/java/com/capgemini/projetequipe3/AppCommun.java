7685fb7a65f8ee6a4c08a95d427f7253ba93871d
Maubert Lucie <luspam@spam.com>
2018-05-16 12:36:16 +0200

    logger

--- projetequipe3/src/main/java/com/capgemini/projetequipe3/AppCommun.java
+++ projetequipe3/src/main/java/com/capgemini/projetequipe3/AppCommun.java
@@ -3,6 +3,7 @@ package com.capgemini.projetequipe3;
 public class AppCommun {
 
 	public static void main(String[] args) {
+		
 		System.out.println("bonjour bonjour");
 		System.out.println("yeah!!!");
 		System.out.println("petite modif sur la branche master");
--- projetequipe3/src/main/java/com/capgemini/projetequipe3/AppLucie.java
+++ projetequipe3/src/main/java/com/capgemini/projetequipe3/AppLucie.java
@@ -1,17 +1,26 @@
 package com.capgemini.projetequipe3;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AppLucie {
+	
+	private static Logger logger = LoggerFactory.getLogger(AppLucie.class);
 
 	public static void main(String[] args) {
-
+		
+		logger.info("création d'une nouvelle branche devLucie");
+		logger.debug("test de modif sur ma branche");
+		logger.info("ouuuuh la belle branche");
+		logger.info("tu veux venir sur ma branche ??");
+		
+		/*
 		System.out.println("création d'une nouvelle branche devLucie");
-
 		System.out.println("test de modif sur ma branche");
-	
 		System.out.println("ouuuuh la belle branche");
-
 		System.out.println("tu veux venir sur ma branche ??");
-
+		*/
+		
 	}
 
 }
